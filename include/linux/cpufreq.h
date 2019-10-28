a4531b4823b82aa842f1817c7788ccfefdb70c58
corsicanu <corsicanu22@gmail.com>
2019-10-28 14:29:01 +0200

    cpufreq: blu_active governor
    Signed-off-by: corsicanu <corsicanu22@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -485,6 +485,9 @@ extern unsigned int cpufreq_interactive_get_hispeed_freq(int cpu);
 #endif
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_PERFORMANCE
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_performance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
