7b54533a41dc49ef6da39e11d778ef2b795bd945
aymanrgab <aymanrar2c@gmail.com>
2022-03-27 16:05:30 +0000

    fixes

--- drivers/input/ff-memless.c
+++ drivers/input/ff-memless.c
@@ -38,10 +38,10 @@ MODULE_AUTHOR("Anssi Hannula <anssi.hannula@gmail.com>");
 MODULE_DESCRIPTION("Force feedback support for memoryless devices");
 
 /* Number of effects handled with memoryless devices */
-#define FF_MEMLESS_EFFECTS	16
+#define FF_MEMLESS_EFFECTS	64
 
 /* Envelope update interval in ms */
-#define FF_ENVELOPE_INTERVAL	50
+#define FF_ENVELOPE_INTERVAL	10
 
 #define FF_EFFECT_STARTED	0
 #define FF_EFFECT_PLAYING	1
--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1299,8 +1299,7 @@ static struct binder_node *binder_init_node_ilocked(
 		FLAT_BINDER_FLAG_SCHED_POLICY_SHIFT;
 	node->min_priority = to_kernel_prio(node->sched_policy, priority);
 	node->accept_fds = !!(flags & FLAT_BINDER_FLAG_ACCEPTS_FDS);
-	node->txn_security_ctx = !!(flags & FLAT_BINDER_FLAG_TXN_SECURITY_CTX);
-	node->inherit_rt = !!(flags & FLAT_BINDER_FLAG_INHERIT_RT);
+	node->txn_security_ctx =0; /* !!(flags & FLAT_BINDER_FLAG_TXN_SECURITY_CTX);*/
 	spin_lock_init(&node->lock);
 	INIT_LIST_HEAD(&node->work.entry);
 	INIT_LIST_HEAD(&node->async_todo);
